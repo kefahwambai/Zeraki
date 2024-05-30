38e65224657cd9c1839da5a7c18a3aedccdb617e
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-05-30 12:57:01 +0300

    added upcoming invoices

--- src/components/UpcomingInvoices/UpcomingInvoices.js
+++ src/components/UpcomingInvoices/UpcomingInvoices.js
@@ -4,25 +4,33 @@ import './upcominginvoices.css';
 
 export default function UpcomingInvoices() {
   const [invoices, setInvoices] = useState([]);
+  const [schools, setSchools] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/invoices?_sort=dueDate&_order=asc')
-      .then(response => setInvoices(response.data))
+    axios.get('https://zerakidb.vercel.app/upcominginvoices')
+      .then(response => {
+        setInvoices(response.data);
+      })
       .catch(error => console.error('Error fetching upcoming invoices:', error));
   }, []);
+  
+  
 
   return (
     <div className="upcoming-invoices">
       <h3>Upcoming Invoices</h3>
       <ul>
-        {invoices.map((invoice, index) => (
-          <li key={index}>
-            <p>{invoice.school}</p>
-            <p>{invoice.amount}</p>
-            <p>{invoice.dueDate}</p>
-            <button>Collect Payment</button>
-          </li>
-        ))}
+        {invoices.map((invoice, index) => {
+          const school = schools.find(school => school.id === invoice.schoolId);
+          return (
+            <li key={index}>
+              <p><span></span> {invoice.name}</p>
+              <p><span>Amount:</span> {invoice.amount}</p>
+              <p><span>Due Date:</span> {invoice.dueDate}</p>
+              <button>Collect Payment</button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
