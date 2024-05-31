378e6a3be10047c3cd89fb6ef8e29167416f71d9
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-05-31 05:17:16 +0300

    modified sidebar ui

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -5,8 +5,8 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <h2>Zeraki Dashboard</h2>
-      <nav>
-        <ul>
+      <nav style={{marginTop: '5rem'}}>
+        <ul style={{marginLeft: '2rem'}}>
           <li><Link to="/">Dashboard</Link></li>
           <li><Link to="/schools">Schools</Link></li>
         </ul>
