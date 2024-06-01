a3a52e8627b44c0050401bdc93e08c57113ca350
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-06-01 22:28:02 +0300

    added Footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      &copy; 2024 <a href="http://www.kefahwambai.com" target="_blank" rel="noopener noreferrer">kefahwambai.com</a>
+      &copy; 2024 <a href="http://www.kefahwambai.com" target="_blank" rel="noopener noreferrer">kefahwambai</a>
     </footer>
   );
 };
